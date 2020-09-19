8818dec019e405323e52762552c87e8a876187c6
ankit0487 <ankitlinux@DESKTOP-UUSO98L.localdomain>
2020-09-19 14:23:44 +0530

    updated metadata

--- floginapp-infra/metadata.rb
+++ floginapp-infra/metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'ankgupta4233@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures floginapp-infra'
 version '0.1.0'
-chef_version '>= 15.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
