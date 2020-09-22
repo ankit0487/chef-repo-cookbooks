1af49f4cc2cb136ba4e390d788dcf8cac014dc81
ankit0487 <ankitlinux@DESKTOP-UUSO98L.localdomain>
2020-09-22 21:34:38 +0530

    fixed issue in gunicorn.conf and removed default recipe

--- floginapp-infra/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook:: floginapp-infra
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
-include_recipe 'floginapp-infra::configure'
-include_recipe 'floginapp-infra::deploy'
